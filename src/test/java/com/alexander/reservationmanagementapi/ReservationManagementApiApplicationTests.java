a8132b20de0ccb525569405a62c21cfec2383fe6
Alexander Klyuev <deposoff@gmail.com>
2026-05-15 22:00:17 +0300

    Initial commit

---
+++ src/test/java/com/alexander/reservationmanagementapi/ReservationManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexander.reservationmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
